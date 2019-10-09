ee9d2c42f0a507e80d21a6b736faf2a283ce5e38
Jay Nabonne <jaynabonne@gmail.com>
2019-10-09 11:27:37 +0100

    DDCNLS-6917: Updated dependency libraries.

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,15 +7,15 @@ object AppDependencies {
   val compile: Seq[ModuleID] = Seq(
     ws,
     "uk.gov.hmrc" %% "microservice-bootstrap" % "10.6.0",
-    "uk.gov.hmrc" %% "frontend-bootstrap"     % "12.8.0",
-    "uk.gov.hmrc" %% "play-ui"                % "7.31.0-play-25",
-    "uk.gov.hmrc" %% "domain"                 % "5.3.0",
-    "uk.gov.hmrc" %% "play-whitelist-filter"  % "2.0.0"
+    "uk.gov.hmrc" %% "frontend-bootstrap"     % "12.9.0",
+    "uk.gov.hmrc" %% "play-ui"                % "8.2.0-play-25",
+    "uk.gov.hmrc" %% "domain"                 % "5.6.0-play-25",
+    "uk.gov.hmrc" %% "play-whitelist-filter"  % "3.1.0-play-25"
   )
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"                       %% "hmrctest"              % "3.4.0-play-25" ,
-    "org.scalatest"                     %% "scalatest"             % "3.0.5",
+    "uk.gov.hmrc"                       %% "hmrctest"              % "3.9.0-play-25" ,
+    "org.scalatest"                     %% "scalatest"             % "3.0.8",
     "org.pegdown"                       %  "pegdown"               % "1.6.0",
     "com.typesafe.play"                 %% "play-test"             % PlayVersion.current,
     "org.scalatestplus.play"            %% "scalatestplus-play"    % "2.0.1",
