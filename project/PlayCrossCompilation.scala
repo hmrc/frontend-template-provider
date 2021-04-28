8034d522f818fc248218efc0d4a33bdf284b7be0
oliwiaKot <48218839+oliwiaKot@users.noreply.github.com>
2021-04-28 17:00:06 +0100

    DDCNL-3943| added play cross compilation

--- project/PlayCrossCompilation.scala
+++ project/PlayCrossCompilation.scala
@@ -1,4 +1,14 @@
 import uk.gov.hmrc.playcrosscompilation.AbstractPlayCrossCompilation
-import uk.gov.hmrc.playcrosscompilation.PlayVersion.Play26
+import uk.gov.hmrc.playcrosscompilation.PlayVersion.{Play26, Play27}
 
-object PlayCrossCompilation extends AbstractPlayCrossCompilation(defaultPlayVersion = Play26)
+object PlayCrossCompilation extends AbstractPlayCrossCompilation(defaultPlayVersion = Play26) {
+  def version: String = playVersion match {
+    case Play26 => "2.6.11"
+    case Play27 => "2.7.5"
+  }
+  override def playCrossScalaBuilds(scalaVersions: Seq[String]): Seq[String] =
+    playVersion match {
+      case Play26 => scalaVersions
+      case Play27 => scalaVersions.filter(version => version.startsWith("2.12"))
+    }
+}
