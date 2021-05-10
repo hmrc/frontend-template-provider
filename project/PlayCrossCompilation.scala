97e77dbafa7090bb3718edef08bba443d50707f7
shikkhasingh <shika.singh@digital.hmrc.gov.uk>
2021-05-10 17:05:35 +0100

    DDCNL-3943 | changing the default version to play2.7

--- project/PlayCrossCompilation.scala
+++ project/PlayCrossCompilation.scala
@@ -1,10 +1,10 @@
 import uk.gov.hmrc.playcrosscompilation.AbstractPlayCrossCompilation
-import uk.gov.hmrc.playcrosscompilation.PlayVersion.{Play26, Play27, Play28}
+import uk.gov.hmrc.playcrosscompilation.PlayVersion.{Play26, Play27}
 
-object PlayCrossCompilation extends AbstractPlayCrossCompilation(defaultPlayVersion = Play26) {
+object PlayCrossCompilation extends AbstractPlayCrossCompilation(defaultPlayVersion = Play27) {
   def version: String = playVersion match {
     case Play26 => "2.6.11"
-    case Play27 => "2.7.5"
+    case Play27 => "2.7.9"
   }
   override def playCrossScalaBuilds(scalaVersions: Seq[String]): Seq[String] =
     playVersion match {
