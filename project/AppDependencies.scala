2c7fa0415269c8c9ccdadf6b33691a483fdf4d9a
shikkhasingh <shika.singh@digital.hmrc.gov.uk>
2021-05-17 11:04:52 +0100

    DDCNL-3943 | review comments

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,7 +8,7 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     ws,
-    "uk.gov.hmrc" %% s"bootstrap-frontend-$playVersion" % "4.3.0",
+    "uk.gov.hmrc" %% s"bootstrap-frontend-$playVersion" % "5.3.0",
     "uk.gov.hmrc" %% "play-ui"                 % s"9.2.0-$playVersion",
     "uk.gov.hmrc" %% "domain"                  % s"5.11.0-$playVersion",
     "uk.gov.hmrc" %% "govuk-template"          % s"5.66.0-$playVersion"
@@ -16,7 +16,7 @@ object AppDependencies {
 
   val test: Seq[ModuleID] = Seq(
     "org.pegdown"                       %  "pegdown"               % "1.6.0",
-    "org.scalatestplus.play"            %% "scalatestplus-play"    % "3.1.3",
+    "org.scalatestplus.play"            %% "scalatestplus-play"    % "4.0.3",
     "com.github.spullara.mustache.java" %  "compiler"              % "0.9.6",
     "com.github.spullara.mustache.java" %  "scala-extensions-2.11" % "0.9.6"
   ).map(_ % "test")
